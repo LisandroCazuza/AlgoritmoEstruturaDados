4ecd49f5fab4031eea355bb8b247d58e32707e99
LisandroCazuza <geohist2005@gmail.com>
2022-03-30 18:38:46 -0300

    lista_produtos_classes

--- lista_produtos_classes.py
+++ lista_produtos_classes.py
@@ -1,8 +1,8 @@
 class ListaProduto(): 
     def __init__(self,produto,preco,quantidade):
       self.prod  =  produto
-        self.preco = valor
-        self.quant= quantidade
+      self.preco = valor
+      self.quant= quantidade
         
         
         
