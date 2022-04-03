2d6b1d1c5a02aba75b70abb87d9b98cca79b1605
LisandroCazuza <geohist2005@gmail.com>
2022-04-03 19:28:24 -0300

    AlgoritmoEstruturaDados

--- classes/produtosclasse.py
+++
@@ -1,8 +0,0 @@
-"""
-from listaprodutosclasses import Funcionarios
-func = Funcionarios(nome = "Pedro", sobrenome =  "Souza",idade =  30,endereco =  "casa", )
-print(f"Nome: {func.get_nome()}") 
-print(f"Sobrenome: {func.get_sobrenome()}")
-print(f"Idade: {func.get_idade()}")
-print(f"Endereco: {func.get_endereco()}")
-"""
