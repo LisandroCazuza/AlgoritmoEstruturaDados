701769aa1203ea6a789e618a51774d8b2ae39271
LisandroCazuza <geohist2005@gmail.com>
2022-04-03 14:08:33 -0300

    lista_produtos_classes

--- lista_produtos_classes.py
+++ lista_produtos_classes.py
@@ -1,5 +1,15 @@
 class Produtos( ):
-      def __int__(set, nome, preco, quantidade):
-            self. n = nome
-            self. p = preco
-           self.q = quantidade
+      def __int__(set, nom, prec, quant):
+            self. __nome = nom
+            self. __preco = prec
+            self. __quantidade = quant
+      def get_nome(self):
+            return self. __nome
+      def get_preco(self):
+            return self.__preco
+      def get_quantidade(self):
+            return self.__quantidade
+      def  __str__(self):
+          
+
+    
