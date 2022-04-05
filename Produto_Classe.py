fc01016216a24d136bd9d99b7f6b36a0ea896078
LisandroCazuza <geohist2005@gmail.com>
2022-04-05 18:56:53 -0300

    AlgoritmoEstruturaDados

--- Produto_Classe.py
+++ Produto_Classe.py
@@ -1,22 +1,13 @@
 class Produtos():
-    def __init__(self,nome, preco, quantidade):
-        self.nome  = nome
-        self.preco = preco
-        self.quantidade = quantidade
-# Busca
-    def get_nome(self):
-        return self.nome
-    def get_preco(self):
-        return self.preco
-    def get_quantidade(self):
-        return self.quantidade
-# Modifica
-def set_nome(self):
-    self.nome = nome
-prod = Produtos("Ana ", 10, 2)
-print(f"O nome para ser trocado: {prod.get_nome()}")
-print( prod.get_preco())
-print(prod.get_quantidade())
-novo = input("Digite um novo nome: ")
-prod.set_nome(novo)
-print(f"O novo nome é: {prod.get_nome()}")
+    def __init__(self,nome, preco, quant):
+        self.__produto  = nome
+        self.__valor = preco
+        self.__quantidade = quant
+        #Buscar informação 
+        def get_produto(self):
+            return self.__produto
+        def get_valor(self):
+            return self.__valor 
+        def get_quantidade(self):
+            return self.__quantidade
+        
