e65d99015912da4b79d6bd016151b295eb655c22
LisandroCazuza <geohist2005@gmail.com>
2022-04-05 19:35:42 -0300

    AlgoritmoEstruturaDados

--- Produto_Classe.py
+++ Produto_Classe.py
@@ -1,19 +1,14 @@
 class Produtos():
-    def __init__(self,nome, preco, quant):
-        self.__produto  = nome
-        self.__valor = preco
-        self.__quantidade = quant
-        #Buscar informação 
-        def get_produto(self):
-            return self.__produto
-        def get_valor(self):
-            return self.__valor 
-        def get_quantidade(self):
-            return self.__quantidade
-        #Modificar informação
-        #def set_produto(self, nome):
-            
-prod = Produtos("Massa", 5, 2)
-print(f"Nome produto: {prod.get_produto()}\n{prod.get_valor()}\n{prod.get_quantidade()}")
+  def __init__(self,nome, valor, quant ):
+      self.__produto = nome 
+      self.__preco = valor 
+      self.__quantidade = quant
+  def get_produto(self):
+      return self.__produto
+  def get_preco(self):
+      return self.__preco
+  def get_quantidade(self):
+      return self.__quantidade
+      
 
         
