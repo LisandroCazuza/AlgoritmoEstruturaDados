d24dad5772d9f633f25d98b6629562e32b33f699
LisandroCazuza <geohist2005@gmail.com>
2022-04-05 21:05:31 -0300

    AlgoritmoEstruturaDados

--- Produtoclasse.py
+++ Produtoclasse.py
@@ -3,12 +3,14 @@ class Produtos():
       self.__produto = nome 
       self.__preco = valor 
       self.__quantidade = quant
+      #Buscar informação 
   def get_produto(self):
       return self.__produto
   def get_preco(self):
       return self.__preco
   def get_quantidade(self):
       return self.__quantidade
+  #Trocar informação
 print("Preço atual do produto: ")
 prod = Produtos("Massa","R$ 5,00", 1)
 print(f"Nome do produto: {prod.get_produto()}\nPreço: {prod.get_preco()}"
