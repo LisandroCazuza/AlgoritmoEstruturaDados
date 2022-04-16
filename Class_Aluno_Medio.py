ea2038dae99ec21c889d5a1dc148c3829d714665
LisandroCazuza <geohist2005@gmail.com>
2022-04-16 18:44:12 -0300

    Heranca

--- Class_Aluno_Medio.py
+++ Class_Aluno_Medio.py
@@ -6,7 +6,7 @@ class Aluno_Media():
     def get_ano(self):
         return self.__ano
 
-alunomedio = Aluno(23 , "Lisandro ", 200511025)
+alunomedio = Aluno(23 , "Lisandro "," 200511025")
 print(f"\n\tCódigo do Aluno: {alunomedio.get_codigo()}"
 f"\n\tNome do aluno:  {alunomedio .get_nome()}"
 f"\n\tNº da matricula do aluno:  {alunomedio .get_matricula()}  ")
