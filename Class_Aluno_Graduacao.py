8b21bba6b020ce311ed84c35ef126526995ff3b7
LisandroCazuza <geohist2005@gmail.com>
2022-04-24 15:00:38 -0300

    Heranca

--- Class_Aluno_Graduacao.py
+++ Class_Aluno_Graduacao.py
@@ -6,11 +6,11 @@ class Aluno_Graduacao(Aluno):
     def get_semestre(self):
         return self.__semestre
 alunagraduacao = Aluno(13, "Ana" ," 2015211034" )
-print(f"\n\tCódigo da aluna: {alunagraduacao.get_codigo()}" 
-f"\n\tNome da aluna: {alunagraduacao.get_nome()}"
-f"\n\tNº da matricula da aluna:  {alunagraduacao .get_matricula()}  ")
+print(f"\nCódigo da aluna: {alunagraduacao.get_codigo()}\n" 
+f"\nNome da aluna: {alunagraduacao.get_nome()}\n"
+f"\nNº da matricula da aluna:  {alunagraduacao .get_matricula()}\n")
 alunagrad = Aluno_Graduacao(3)
-print(f"\tA aluna {alunagraduacao.get_nome()} está: {alunagrad .get_semestre()}"
+print(f"\nA aluna {alunagraduacao.get_nome()} está: {alunagrad .get_semestre()}"
  f"º semestre  de graduação ")
         
         
